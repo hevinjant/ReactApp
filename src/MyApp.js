86975285642a039c12862477e6a49e2dd7c71c84
Hevin Jant <hevin.jant@gmail.com>
2021-09-25 00:26:10 -0700

    added state for MyApp

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import Table from './Table'
 
-const characters = [
+// const characters = [
+//       {
+//         name: 'Charlie',
+//         job: 'Janitor',
+//       },
+//       {
+//         name: 'Mac',
+//         job: 'Bouncer',
+//       },
+//       {
+//         name: 'Dee',
+//         job: 'Aspring actress',
+//       },
+//       {
+//         name: 'Dennis',
+//         job: 'Bartender',
+//       },
+// ];
+
+function MyApp() {
+    const [characters, setCharacters] = useState([
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -18,12 +38,18 @@ const characters = [
         name: 'Dennis',
         job: 'Bartender',
       },
-];
+    ]);
 
-function MyApp() {
+    function removeOneCharacter(index) {
+      const updated = characters.filter((character, i) => {
+        return i !== index
+      });
+      setCharacters(updated);
+    }
+
     return (
       <div className="container">
-            <Table characterData={characters} />
+            <Table characterData={characters} removeCharacter={removeOneCharacter}/>
       </div>
     );
 }
